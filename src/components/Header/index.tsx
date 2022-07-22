e2c3b59a898956df55d86bdefd1a914a939e9700
Paulo301 <paulovictor1918@gmail.com>
2022-07-22 01:24:25 -0300

    feat(Header): hide menu outside home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useLocation } from "react-router-dom";
+
 import { BiUserCircle } from 'react-icons/bi';
 import logoImg from '../../assets/logo.png';
 
@@ -10,20 +12,24 @@ interface HeaderProps {
 }
 
 export function Header(props: HeaderProps) {
+  const { pathname } = useLocation();
 
   return (
     <Container>
       <div>
         <img src={logoImg} alt="ClickBeard" />
       </div>
-
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#servicos">Serviços</a></li>
-          <li><a href="#contato">Contato</a></li>
-        </ul>
-      </nav>
+      {
+        pathname === '/' && (
+          <nav>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#servicos">Serviços</a></li>
+              <li><a href="#contato">Contato</a></li>
+            </ul>
+          </nav>
+        )
+      }
 
       <IconButton icon={BiUserCircle} size={36} />
     </Container>
